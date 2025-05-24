f40933ef8659fd40e436a3ffb984bc1ea919d4c3
ulisseso2 <ulissesrce@gmail.com>
2025-05-24 15:36:49 -0300

    remove cache

--- utils/sql_loader.py
+++ utils/sql_loader.py
@@ -25,15 +25,3 @@ def carregar_dados(caminho_sql):
     else:
         st.error("Erro ao conectar ao banco de dados.")
         return pd.DataFrame()
-
-@st.cache_data(tlt=43200)
-def carregar_dados_12hs(caminho_sql):
-    query = carregar_sql(carregar_sql)
-    conexao = conectar_mysql()
-    if conexao:
-        df = pd.read_sql(query, conexao)
-        conexao.close()
-        return df
-    else:
-        st.error("Erro ao carregar os dados")
-        return pd.DataFrame()
